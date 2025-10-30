c3ec4fe0889a542f0c5f7b90e060f979590ae441
JosephKamau <josef@Joe.local>
2025-10-30 12:18:14 +0300

    updated code

--- main.tf
+++ main.tf
@@ -5,36 +5,6 @@ provider "google" {
   zone    = var.zone
 }
 
-### NETWORK
-data "google_compute_network" "default" {
-  name                    = "default"
-}
-
-## SUBNET
-resource "google_compute_subnetwork" "subnet-1" {
-  name                     = var.subnet-name
-  ip_cidr_range            = var.subnet-cidr
-  network                  = data.google_compute_network.default.self_link
-  region                   = var.region
-  private_ip_google_access = var.private_google_access
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = data.google_compute_network.default.self_link
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = var.firewall-ports
-  }
-
-  source_tags = var.compute-source-tags
-}
-
 ### COMPUTE
 ## NGINX PROXY
 resource "google_compute_instance" "nginx_instance" {
@@ -44,7 +14,7 @@ resource "google_compute_instance" "nginx_instance" {
     environment = var.environment_map[var.target_environment]
   }
   tags = var.compute-source-tags
-
+ 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -60,63 +30,16 @@ resource "google_compute_instance" "nginx_instance" {
   }
 }
 
-# WEB1
-resource "google_compute_instance" "web1" {
-  name         = "web1"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB2
-resource "google_compute_instance" "web2" {
-  name         = "web2"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB3
-resource "google_compute_instance" "web3" {
-  name         = "web3"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+module "webservers" {
+  source = "./modules/webservers"
+  project_id = var.project-id
+  server_settings = var.environment_instance_settings
+  region = var.region
+  zone = var.zone
+  network_interface = {
+      network = data.google_compute_network.default.self_link,
+      subnetwork = google_compute_subnetwork.subnet-1.self_link
+      }
 }
 
 ## DB
--- output.tf
+++ output.tf
@@ -1,8 +1,12 @@
-output "subnet-gateway" {
-    value = google_compute_subnetwork.subnet-1.gateway_address
-}
-
-
+### OUTPUTS ###
 output "nginx-public-ip" {
     value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
+}
+
+output "webserver-ips" {
+    value = module.webservers.webserver-ips
+}
+
+output "db-private-ip" {
+    value = google_compute_instance.mysqldb.network_interface[0].network_ip
 }
